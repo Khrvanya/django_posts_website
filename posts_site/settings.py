8f59dae4e60fb8cc6852f4e86acec410fd476f0e
khrvanya@gmail.com <61050608+Khrvanya@users.noreply.github.com>
2020-09-01 23:19:47 +0300

    Using my file for media storage

--- posts_site/settings.py
+++ posts_site/settings.py
@@ -135,18 +135,20 @@ DATABASES['default'].update(db_from_env)
 
 
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'posts_site/static'),
+]
+
 AWS_ACCESS_KEY_ID = 'AKIA2RGPFSONSYO7QHVF'
 AWS_SECRET_ACCESS_KEY = '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'
-AWS_STORAGE_BUCKET_NAME = 'khrvanya-static'
+AWS_STORAGE_BUCKET_NAME = 'khrvanya-assets'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-AWS_DEFAULT_ACL = None
 AWS_S3_OBJECT_PARAMETERS = {
     'CacheControl': 'max-age=86400',
 }
 AWS_LOCATION = 'static'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'posts_site/static'),
-]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+DEFAULT_FILE_STORAGE = 'posts_site.storage_backends.MediaStorage'
