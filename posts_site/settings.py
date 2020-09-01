a836144376b4ebd63efe6f890826ccb1282c02b2
khrvanya@gmail.com <61050608+Khrvanya@users.noreply.github.com>
2020-09-01 15:51:34 +0300

    host settings

--- posts_site/settings.py
+++ posts_site/settings.py
@@ -137,6 +137,8 @@ DATABASES['default'].update(db_from_env)
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
