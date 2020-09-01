935c2d044d54f5f37d83e2f78d189805a0b6be44
khrvanya@gmail.com <61050608+Khrvanya@users.noreply.github.com>
2020-09-01 21:34:24 +0300

    Using my file for media storage

--- posts_site/urls.py
+++ posts_site/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('posts/', include('posts.urls')),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
