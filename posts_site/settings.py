864c70a4979ae18625e6b1f99019d51a9d338494
khrvanya@gmail.com <61050608+Khrvanya@users.noreply.github.com>
2020-09-02 03:18:23 +0300

    Using my file for media storage

--- posts_site/settings.py
+++ posts_site/settings.py
@@ -146,19 +146,24 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
-#AWS_S3_OBJECT_PARAMETERS = {
-#    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
-#    'CacheControl': 'max-age=94608000',
-#}
+AWS_S3_OBJECT_PARAMETERS = {
+    'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+    'CacheControl': 'max-age=94608000',
+}
 
-#AWS_STORAGE_BUCKET_NAME = 'khrvanya-static'
-#AWS_S3_REGION_NAME = 'us-east-2'  
-#AWS_ACCESS_KEY_ID = 'AKIA2RGPFSONQXGLH7NW'
+AWS_STORAGE_BUCKET_NAME = 'khrvanya-static'
+AWS_S3_REGION_NAME = 'us-east-2'  
+AWS_ACCESS_KEY_ID = 'AKIA2RGPFSONQXGLH7NW'
 #AWS_SECRET_ACCESS_KEY = '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'
 
 # Tell django-storages the domain to use to refer to static files.
-#AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 
 # Tell the staticfiles app to use S3Boto3 storage when writing the collected static files (when
 # you run `collectstatic`).
 # STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+
+
+MEDIAFILES_LOCATION = 'media'
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
